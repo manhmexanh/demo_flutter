3da8be25e25402c68895e285b1890f01fc9b2c24
kiennpt <nguyenphuctrungkien@gmail.com>
2019-02-19 17:23:04 +0700

    demo MVP search bar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,46 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:alive/screens/home/home_presenter.dart';
 import 'package:alive/model/word.dart';
+import 'dart:convert';
+import 'package:alive/components/search_comp.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => HomeScreenState();
 }
-class HomeScreenState extends State<HomeScreen> implements HomeView {
 
+class HomeScreenState extends State<HomeScreen> implements HomeView {
   HomePresenter _presenter;
-  
+  Widget appBarTitle = new Text(
+    "Search Example",
+    style: new TextStyle(color: Colors.white),
+  );
+  Icon icon = new Icon(
+    Icons.search,
+    color: Colors.white,
+  );
+
   @override
   void initState() {
     _presenter = new HomePresenter(this);
     _presenter.loadData();
   }
 
+  void callBackTextSearch(String text) {
+    _presenter.getSuggesstion(text);
+  }
+
   @override
-  void showSuguesstions(List<Word> words){
-    print('showSuguesstions ${words.length}');
+  Widget showSuguesstions(List<Word> words) {
+    return new Row(
+      children: words.map((word) => new Text(word.text)).toList(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(),
-      body: _createBodyView(),
+      appBar: buildAppBar(context),
+      body: new Column(
+        children: <Widget>[
+          Text("Dictionary"),
+          new Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SearchBarComp(
+              callBackSearchText: this.callBackTextSearch,
+            ),
+          )
+        ],
+      ),
     );
   }
 
-  Widget _createBodyView() {
-    return new Scaffold(
-      body: new Center(
-        child: TextField(
-          onChanged: (text) => _presenter.getSuggesstion(text),
-          onSubmitted: (text) => _presenter.doSearch(text),
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Please enter a search term'
-          ),
-        )
-      )
+  // Build appbar
+  Widget buildAppBar(BuildContext context) {
+    return new AppBar(
+      centerTitle: true,
+      title: appBarTitle,
     );
   }
 }
